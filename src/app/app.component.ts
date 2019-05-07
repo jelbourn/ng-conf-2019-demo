9b7993479441210eadf9fff3180fd84335290412
Jeremy Elbourn <jelbourn@google.com>
2019-05-07 15:22:37 -0700

    Done with live coding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  todo = [
+    'Write outline',
+    'Make slides',
+    'Script demo',
+    'Practice',
+    'Practice some more',
+    'Do the talk',
+  ];
 
+  done = [
+    'Get dressed',
+  ];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(this.todo, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+         event.previousContainer.data,
+         event.container.data,
+         event.previousIndex,
+         event.currentIndex);
+    }
+  }
 }
